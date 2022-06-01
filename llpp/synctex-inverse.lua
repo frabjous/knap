343959ecf532e7d7bba5debc3c4433d63be7fe5e
Kevin Klement <klement@umass.edu>
2022-05-31 22:50:04 -0400

    quickpush 2022-05-31 22:50:04

--- llpp/synctex-inverse.lua
+++ llpp/synctex-inverse.lua
@@ -1,4 +1,24 @@
 #!/usr/bin/luajit
+---------------------------------------
+-- SyncTeX Inverse Function for LLPP --
+---------------------------------------
+--[[
+    Lua script for relaying SyncTeX search info from llpp to neovim
+    Copyright (C) 2019–2022 Kevin C. Klement
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    ]]--
 
 -- read arguments
 local filename = arg[1]
@@ -68,9 +88,9 @@ end
 -- close process
 proc:close()
 
--- fire up headliness nvim instance to do jump
+-- fire up headless neovim instance to do jump
 local cmd = 'nvim --headless -es --cmd "lua require(\'knaphelper\')' ..
-    '.relayjump(\'all\',\'' .. infile .. '\',' .. linenum .. ',' 
+    '.relayjump(\'all\',\'' .. infile .. '\',' .. linenum .. ','
     .. col .. ')"'
 local succ = os.execute(cmd)
 os.exit(succ)
